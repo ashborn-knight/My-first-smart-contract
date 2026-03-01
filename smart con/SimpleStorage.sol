de2fe2b5239922537ae346cc2bcab60f81fec180
Kenneth Kobina Essien Aidoo <essienkent@gmail.com>
2026-03-01 23:22:05 +0000

    Embedding a smart contract into another smart contract

--- smart con/SimpleStorage.sol
+++ smart con/SimpleStorage.sol
@@ -3,19 +3,30 @@ pragma solidity >=0.8.19 < 0.9.0; //stating version
 
 contract SimpleStorage {
   //  bool hasFavoriteNumber = true;
-    uint256 public favoriteNumber;
-    //string favoriteNumberIntext = "eighty-eight";
-    //int256 favoriteInt = -88;
-    //address myAddress = ;
-    //bytes32  favoriteBytes32 = "cat";
-    
+    uint256  myFavoriteNumber;
+   
+   // uint256[] listOfFavoriteNumbers;
+   struct Person {
+    uint256 favoriteNumber;
+    string name;
+   }
+   
+   Person[] public listOfPeople;
+   
+   mapping(string => uint256) public nameToFavoriteNumber;   
 function store(uint256 _favoriteNumber) public{
-    favoriteNumber = _favoriteNumber;
+    myFavoriteNumber = _favoriteNumber;
    //0xd9145CCE52D386f254917e481eB44e9943F39138
 }
 
 //view , pure
 function retrieve() public view returns(uint256){
-    return favoriteNumber;
+    return myFavoriteNumber;
+}
+
+function addPerson(string memory _name, uint256 _favoriteNumber) public {
+ // Person memory newPerson = Person {_favoriteNumber, _name};
+  listOfPeople.push( Person (_favoriteNumber, _name) );
+  nameToFavoriteNumber [_name] = _favoriteNumber;
 }
 }
---
+++ smart con/StorageFactory.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+
+contract SimpleStorage {
+  //  bool hasFavoriteNumber = true;
+    uint256  myFavoriteNumber;
+   
+   // uint256[] listOfFavoriteNumbers;
+   struct Person {
+    uint256 favoriteNumber;
+    string name;
+   }
+   
+   Person[] public listOfPeople;
+   
+   mapping(string => uint256) public nameToFavoriteNumber;   
+function store(uint256 _favoriteNumber) public{
+    myFavoriteNumber = _favoriteNumber;
+   //0xd9145CCE52D386f254917e481eB44e9943F39138
+}
+
+//view , pure
+function retrieve() public view returns(uint256){
+    return myFavoriteNumber;
+}
+
+function addPerson(string memory _name, uint256 _favoriteNumber) public {
+ // Person memory newPerson = Person {_favoriteNumber, _name};
+  listOfPeople.push( Person (_favoriteNumber, _name) );
+  nameToFavoriteNumber [_name] = _favoriteNumber;
+}
+}
+
+contract StorageFactory{
+    //uint256 public favouriteNumber
+    //type visibility name
+
+    function createSimpleStorageContract () public {
+
+    }
+
+}
