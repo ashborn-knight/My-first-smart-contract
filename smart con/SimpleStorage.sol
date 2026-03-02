4baa6ba33b13ed1d05beaadf16b4226adddb4874
Kenneth Kobina Essien Aidoo <essienkent@gmail.com>
2026-03-02 21:17:43 +0000

    Importing another contract

--- smart con/SimpleStorage.sol
+++ smart con/SimpleStorage.sol
@@ -1,5 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.19 < 0.9.0; //stating version
+// pragma solidity ^0.8.0;
+// pragma solidity >=0.8.0 <0.9.0;
 
 contract SimpleStorage {
   //  bool hasFavoriteNumber = true;
