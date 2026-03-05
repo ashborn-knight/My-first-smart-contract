ab0478c692d482b3749725624ae2389309c7b41e
Kenneth Kobina Essien Aidoo <essienkent@gmail.com>
2026-03-05 00:24:55 +0000

    should work

--- smart con/StorageFactory.sol
+++ smart con/StorageFactory.sol
@@ -2,23 +2,30 @@
 
 pragma solidity ^0.8.18;
 
-import {SimpleStorage, SimpleStorage2} from "./SimpleStorage.sol";
+import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory{
     //uint256 public favouriteNumber
     //type visibility name
    // SimpleStorage public simpleStorage;
    SimpleStorage[] public listOfSimpleStorageContracts;
+   //address[] public listOfSimpleStorageContracts;
 
     function createSimpleStorageContract () public {
         SimpleStorage newSimpleStorageContract = new SimpleStorage();
         listOfSimpleStorageContracts.push(newSimpleStorageContract);
    
     }
-    function sfStore (uint256) _simplestorageIndex, uint256 _newSimpleStorageNumber) public{
+    function sfStore (uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public{
         //Address
         //ABI   - Application Binary Interface
-        SimpleStorage mySimpleStorage = listOfSimpleStorageContract[_simpleStorageIndex];
+        SimpleStorage mySimpleStorage = SimpleStorage(listOfSimpleStorageContracts[_simpleStorageIndex]);
+        mySimpleStorage.store(_newSimpleStorageNumber);
+        
     }
 
+     function sfGet (uint256 _simpleStorageIndex) public view returns(uint256) {
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        return mySimpleStorage.retrieve();
+     }
 }
